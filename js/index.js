679857946016193f4b8e507666f1a13684e0048b
“Martin <martin@forceandform.com>
2024-12-13 10:58:36 -0500

    Completed experiment inside of index.js and commented out y code upon comlpetion. Completed Part 1 of the tasks and updated README accordingly.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/* 
+
+const firstName = "Martin";
+
+console.log(firstName);
+
+firstName = "Joe";
+console.log(firstName);
+
+let bestFriend = "Jeremy";
+console.log(bestFriend);
+
+bestFriend = "Drusie";
+console.log(bestFriend);
+
+*/
+
